6ca2a149908c273d1736e5533700cf9410245535
Nathalie Germain <71288067+LMD-nat@users.noreply.github.com>
2022-08-15 20:20:41 -0400

    Create machine_learning.R

---
+++ assessments/machine_learning.R
@@ -0,0 +1,6 @@
+### You are analyzing data from stock markets and notice that some stocks seem to have a high covariance. 
+### You want to reduce the variables to those that explain most of the variability in the data.
+### Which technique would you use to do that?
+
+print("Principal component analysis")
+
