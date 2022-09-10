95afc52c6da6805b9f14d0af3fb3ed361085b2cc
Nathalie Germain <71288067+LMD-nat@users.noreply.github.com>
2022-09-10 10:45:14 -0400

    Update imports.R

--- code/imports.R
+++ code/imports.R
@@ -1,3 +1,4 @@
+### Basic Import Functions ###
 ### 1.1 ### ------------------------------------------------------------------------ ###
 
 # Import swimming_pools.csv: pools
@@ -335,4 +336,75 @@ removeSheet(my_book, 4)
 
 # Save workbook to "clean.xlsx"
 saveWorkbook(my_book, file = "clean.xlsx")
+               
+### Intermediate Imports ###
+### 2.1 ### Imports from Databases ------------------------------------------------------------------------ ###               
+
+# Load the DBI package
+library(DBI)
+
+# Edit dbConnect() call
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+               
+# Load the DBI package
+library(DBI)
+
+# Connect to the MySQL database: con
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+
+# Build a vector of table names: tables
+tables <- dbListTables(con)
+
+# Display structure of tables
+str(tables)
+               
+# Load the DBI package
+library(DBI)
+
+# Connect to the MySQL database: con
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+
+# Import the users table from tweater: users
+users <- dbReadTable(con, "users")
+
+# Print users
+users
+               
+# Load the DBI package
+library(DBI)
+
+# Connect to the MySQL database: con
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+
+# Get table names
+table_names <- dbListTables(con)
+
+# Import all tables
+tables <- lapply(table_names, dbReadTable, conn = con)
+
+# Print out tables
+tables
+               
+### 2.2 ### 
+### Importing data from databases ------------------------------------------------------------------------ ###                 
 
